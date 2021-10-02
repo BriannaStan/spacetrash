20813d1aec07bdae7888b1d671b563dd892d51d3
BriannaStan <67191768+BriannaStan@users.noreply.github.com>
2021-10-02 09:18:53 +0300

    Initial commit

---
+++ src/main/java/org/hypertree/spacetrash/SpacetrashApplication.java
@@ -0,0 +1,13 @@
+package org.hypertree.spacetrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacetrashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacetrashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hypertree/spacetrash/SpacetrashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hypertree.spacetrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacetrashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
